210c0ea2ef43000cf92a1b0c85c9efed7c26ef06
Shane Pearson <bubbafoley@gmail.com>
2024-04-11 09:53:05 -0500

    add helmet security

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const helmet      = require('helmet');
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -16,6 +17,9 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// security!
+app.use(helmet());
 
 //Index page (static HTML)
 app.route('/')
