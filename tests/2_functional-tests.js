8509291e9f32a0e7972c96834e148f511cb1a6e6
Shane Pearson <bubbafoley@gmail.com>
2024-04-12 15:11:00 -0500

    add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,98 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  test("GET /api/stock-prices - should return stock data for a single stock", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL" })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.propertyVal(res.body.stockData, "stock", "AAPL");
+        assert.property(res.body.stockData, "price");
+        assert.propertyVal(res.body.stockData, "likes", 0);
+        done();
+      });
+  });
 
+  test("GET /api/stock-prices - should view one stock and like it", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL", like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.propertyVal(res.body.stockData, "stock", "AAPL");
+        assert.property(res.body.stockData, "price");
+        assert.propertyVal(res.body.stockData, "likes", 1);
+        done();
+      });
+  });
+
+  test("GET /api/stock-prices - should view one stock and attempt to like it again", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: "AAPL", like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.propertyVal(res.body.stockData, "stock", "AAPL");
+        assert.property(res.body.stockData, "price");
+        assert.propertyVal(res.body.stockData, "likes", 1);
+        done();
+      });
+  });
+
+  test("GET /api/stock-prices - should return stock data for multiple stocks", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["AAPL", "GOOGL"] })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.propertyVal(res.body.stockData[0], "stock", "AAPL");
+        assert.property(res.body.stockData[0], "price");
+        assert.property(res.body.stockData[0], "rel_likes");
+        assert.propertyVal(res.body.stockData[1], "stock", "GOOGL");
+        assert.property(res.body.stockData[1], "price");
+        assert.property(res.body.stockData[1], "rel_likes");
+        done();
+      });
+  });
+
+  test("GET /api/stock-prices - should view two stocks and like them", function (done) {
+    chai
+      .request(server)
+      .get("/api/stock-prices")
+      .query({ stock: ["AAPL", "GOOGL"], like: true })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.property(res.body, "stockData");
+        assert.isArray(res.body.stockData);
+        assert.lengthOf(res.body.stockData, 2);
+        assert.propertyVal(res.body.stockData[0], "stock", "AAPL");
+        assert.property(res.body.stockData[0], "price");
+        assert.property(res.body.stockData[0], "rel_likes");
+        assert.propertyVal(res.body.stockData[1], "stock", "GOOGL");
+        assert.property(res.body.stockData[1], "price");
+        assert.property(res.body.stockData[1], "rel_likes");
+        done();
+      });
+  });
 });
